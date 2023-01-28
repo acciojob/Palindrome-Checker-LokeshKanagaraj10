29d20bfd2e402035920cacd7f8b1a70925ea715d
LokeshKanagaraj10 <120785781+LokeshKanagaraj10@users.noreply.github.com>
2023-01-28 23:38:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // complete the given function
 
 function palindrome(str){
+	const n = str.length
+	const arr = str.split("")
+	let i = 0;
+	let j = n-1;
+	if(i<j)
+	{
+	if(arr[i] == arr[j])
+		{
+			i++;
+			j--;
+			if(i===j){
+				return true
+			}
+		}
+		else{
+			return false
+		}
+	}
+
 
 }
 module.exports = palindrome
