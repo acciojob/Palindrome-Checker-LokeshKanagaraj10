d0506967f3897be19c147a9556607acdb9fff5a9
LokeshKanagaraj10 <120785781+LokeshKanagaraj10@users.noreply.github.com>
2023-01-29 00:28:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-	const n = str.length
+		const n = str.length
 	const arr = str.toLowerCase().split("")
-	let i = 0;
-	let j = n-1;
-	while(i<j){
-		if(arr[i]==arr[j]){
-			if(i>=j){
-				return true;
-			}
-			i++;
-			j--;
-		}
-		else{
-			return false;
-		}
+	if(arr.join() == arr.reverse().join()){
+		return true
 	}
-
+	else{
+	  return false
+	}
 
 }
 module.exports = palindrome
